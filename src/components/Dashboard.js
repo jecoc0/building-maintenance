377483a27e45a22178df27c0070e72906ba5cbee
BryceStockman <65779581+BryceStockman@users.noreply.github.com>
2022-03-29 21:04:50 -0600

    header, dashboard, page, footer components setup

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,49 +1,19 @@
 import React, { useState } from 'react';
-import { Card, Button, Alert } from 'react-bootstrap';
-import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
-import Header from '../components/Header';
-import Nav from '../components/Nav';
+import Header from './Header';
+import Nav from './Nav';
+import Page from './Page';
+import Footer from './Footer';
 
 export default function Dashboard() {
-  const [error, setError] = useState('');
-  const { currentUser, logout } = useAuth();
-  const navigate = useNavigate();
-  const [currentPage, setCurrentPage] = useState('/')
+  const [currentPage, setCurrentPage] = useState('/');
 
-  async function handleLogout() {
-    setError('');
-
-    try {
-      await logout();
-      navigate('/login');
-    } catch {
-      setError('Failed to log out');
-    }
-  }
   return (
     <>
       <Header>
         <Nav setCurrentPage={setCurrentPage}></Nav>
       </Header>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Profile</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <strong>Email: </strong> {currentUser?.email}
-          <Link
-            to="/update-profile"
-            className="btn btn-outline-danger w-100 mt-3"
-          >
-            Update Profile
-          </Link>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        <Button type="button" className="btn btn-danger" onClick={handleLogout}>
-          Log Out
-        </Button>
-      </div>
+      <Page currentPage={currentPage}></Page>
+      <Footer></Footer>
     </>
   );
 }
