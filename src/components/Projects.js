a84a69747f223ecfd487b63b1813e394c288d04d
BryceStockman <65779581+BryceStockman@users.noreply.github.com>
2022-03-30 15:42:21 -0600

    updated handleSubmit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,130 +1,106 @@
 import React from 'react';
 
 function Projects() {
-  const [image, setImage] = React.useState("");
-  const [name, setBuilding] = React.useState("");
-  const [address, setAddress] = React.useState("");
-  const [address2, setAddress2] = React.useState("");
-  const [city, setCity] = React.useState("");
-  const [state, setState] = React.useState("");
-  const [zip, setZip] = React.useState("");
-  const [todo, setToDo] = React.useState("");
+  const [image, setImage] = React.useState('');
+  const [name, setBuilding] = React.useState('');
+  const [address, setAddress] = React.useState('');
+  const [address2, setAddress2] = React.useState('');
+  const [city, setCity] = React.useState('');
+  const [state, setState] = React.useState('');
+  const [zip, setZip] = React.useState('');
+  const [todo, setToDo] = React.useState('');
   {
     // const uploadBtn = document.querySelector('#btn_upload');
-  // const finishBtn = document.querySelector('#finishBtn');
-  // const fileInput = document.querySelector('#file');
-  // const buildingInput = document.querySelector('#inputBuilding');
-  // const addressInput = document.querySelector('#inputAddress');
-  // const addressInput2 = document.querySelector('#inputAddress2');
-  // const cityInput = document.querySelector('#inputCity');
-  // const stateInput = document.querySelector('#inputState');
-  // const zipInput = document.querySelector('#inputZip');
-  // const toDoInput = document.querySelector('#inputToDo');
-  };
-  
+    // const finishBtn = document.querySelector('#finishBtn');
+    // const fileInput = document.querySelector('#file');
+    // const buildingInput = document.querySelector('#inputBuilding');
+    // const addressInput = document.querySelector('#inputAddress');
+    // const addressInput2 = document.querySelector('#inputAddress2');
+    // const cityInput = document.querySelector('#inputCity');
+    // const stateInput = document.querySelector('#inputState');
+    // const zipInput = document.querySelector('#inputZip');
+    // const toDoInput = document.querySelector('#inputToDo');
+  }
 
   let project = {};
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      // const selectedFile = fileInput.files[0];
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // const selectedFile = fileInput.files[0];
 
-      console.log(image);
-      console.log(name);
-      console.log(address);
-      console.log(address2);
-      console.log(city);
-      console.log(state);
-      console.log(zip);
-      console.log(todo);
+    console.log(image);
+    console.log(name);
+    console.log(address);
+    console.log(address2);
+    console.log(city);
+    console.log(state);
+    console.log(zip);
+    console.log(todo);
 
-      project.buildingImage = image;
-      project.buildingName = name;
-      project.buildingAddress = address;
-      project.buildingAddress2 = address2;
-      project.buildingCity = city;
-      project.buildingState = state;
-      project.buildingZip = zip;
-      project.buildingToDo = todo;
+    project.buildingImage = image;
+    project.buildingName = name;
+    project.buildingAddress = address;
+    project.buildingAddress2 = address2;
+    project.buildingCity = city;
+    project.buildingState = state;
+    project.buildingZip = zip;
+    project.buildingToDo = todo;
 
-      console.log(project);
+    console.log(project);
 
-      return (
-        <div className="card mb-3">
-            <div className="row no-gutters">
-              <div className="col-md-4">
-                <img
-                  src="sample-house.jpg"
-                  className="card-img"
-                  alt="..."
-                />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">The Johnson's</h5>
-                  <a
-                    href="https://maps.google.com/?q=6522%20S%20Big%20Cottonwood%20Canyon%20Rd%20%23200%2C%20Holladay%2C%20UT%2084121"
-                    className="card-text"
-                  >
-                    6522 S Big Cottonwood Canyon Rd #200, Holladay, UT 84121
-                  </a>
-                  <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Esse, eius tempora. Veniam cumque doloribus labore
-                    repudiandae rerum earum accusantium nemo reiciendis
-                    molestiae, fuga quisquam architecto magni iusto quas
-                    sapiente officiis minus, eligendi quaerat commodi
-                    exercitationem harum nam optio? Asperiores provident
-                    temporibus in voluptatum optio similique iste omnis quo
-                    laborum doloremque!
-                  </p>
-                  <p className="card-text">
-                    <small className="text-muted">
-                      Last updated 3 mins ago
-                    </small>
-                  </p>
-                  <h6 className="card-subtitle mb-2">Tasks To-Do</h6>
-                  <ul className="list-group list-group-flush">
-                    <li className="list-group-item">Bathroom</li>
-                    <li className="list-group-item">Cabinets</li>
-                    <li className="list-group-item">Landscaping</li>
-                  </ul>
-                </div>
-                <div className="d-flex px-4 mb-3">
-                  <div className="dropdown mr-1">
-                    <button
-                      type="button"
-                      className="btn btn-secondary dropdown-toggle"
-                      id="dropdownMenuOffset"
-                      data-toggle="dropdown"
-                      aria-haspopup="true"
-                      aria-expanded="false"
-                      data-offset="10,20"
-                    >
-                      Status
-                    </button>
-                    <div
-                      className="dropdown-menu"
-                      aria-labelledby="dropdownMenuOffset"
-                    >
-                      <div className="dropdown-item text-success">
-                        Completed
-                      </div>
-                      <div className="dropdown-item text-info">
-                        In Progress
-                      </div>
-                      <div className="dropdown-item text-warning">
-                        Bidded
-                      </div>
-                    </div>
-                  </div>
+    return (
+      <div className="card mb-3">
+        <div className="row no-gutters">
+          <div className="col-md-4">
+            <img src={project.buildingImage} className="card-img" alt="..." />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">{project.buildingName}</h5>
+              <a
+                href="https://maps.google.com/?q=6522%20S%20Big%20Cottonwood%20Canyon%20Rd%20%23200%2C%20Holladay%2C%20UT%2084121"
+                className="card-text"
+              >
+                {project.buildingAddress}
+              </a>
+              <p>{project.buildingDescription}</p>
+              <p className="card-text">
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </p>
+              <h6 className="card-subtitle mb-2">Tasks To-Do</h6>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">{todo}</li>
+              </ul>
+            </div>
+            <div className="d-flex px-4 mb-3">
+              <div className="dropdown mr-1">
+                <button
+                  type="button"
+                  className="btn btn-secondary dropdown-toggle"
+                  id="dropdownMenuOffset"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                  data-offset="10,20"
+                >
+                  Status
+                </button>
+                <div
+                  className="dropdown-menu"
+                  aria-labelledby="dropdownMenuOffset"
+                >
+                  <div className="dropdown-item text-success">Completed</div>
+                  <div className="dropdown-item text-info">In Progress</div>
+                  <div className="dropdown-item text-warning">Bidded</div>
                 </div>
               </div>
             </div>
           </div>
-      );
-    };
-  
+        </div>
+      </div>
+    );
+  };
+
   return (
     <main>
       {/* Project Modal */}
@@ -160,7 +136,12 @@ function Projects() {
                 </div>
                 {/* Modal content */}
                 <div className="modal-body">
-                  <form method="post" action="" enctype="multipart/form-data" onSubmit={handleSubmit}>
+                  <form
+                    method="post"
+                    action=""
+                    enctype="multipart/form-data"
+                    onSubmit={handleSubmit}
+                  >
                     <div className="form-row">
                       <div className="form-group">
                         {/* file upload */}
@@ -171,7 +152,7 @@ function Projects() {
                           id="file"
                           className="form-control"
                           value={image}
-                          onChange={e => setImage(e.target.value)}
+                          onChange={(e) => setImage(e.target.value)}
                         />
                         <br />
                         <input
@@ -191,7 +172,7 @@ function Projects() {
                           id="inputBuilding"
                           placeholder="Government Building"
                           value={name}
-                          onChange={e => setBuilding(e.target.value)}
+                          onChange={(e) => setBuilding(e.target.value)}
                         />
                       </div>
                     </div>
@@ -203,7 +184,7 @@ function Projects() {
                         id="inputAddress"
                         placeholder="1234 Main St"
                         value={address}
-                        onChange={e => setAddress(e.target.value)}
+                        onChange={(e) => setAddress(e.target.value)}
                       />
                     </div>
                     <div className="form-group">
@@ -214,7 +195,7 @@ function Projects() {
                         id="inputAddress2"
                         placeholder="Apartment, studio, or floor"
                         value={address2}
-                        onChange={e => setAddress2(e.target.value)}
+                        onChange={(e) => setAddress2(e.target.value)}
                       />
                     </div>
                     <div className="form-row">
@@ -225,13 +206,17 @@ function Projects() {
                           className="form-control"
                           id="inputCity"
                           value={city}
-                          onChange={e => setCity(e.target.value)}
+                          onChange={(e) => setCity(e.target.value)}
                         />
                       </div>
                       <div className="form-group col-md-4">
                         <label for="inputState">State</label>
-                        <select id="inputState" className="form-control" value={state}
-                          onChange={e => setState(e.target.value)}>
+                        <select
+                          id="inputState"
+                          className="form-control"
+                          value={state}
+                          onChange={(e) => setState(e.target.value)}
+                        >
                           <option selected>Choose...</option>
                           <option>Alabama</option>
                           <option>Alaska</option>
@@ -292,7 +277,7 @@ function Projects() {
                           className="form-control"
                           id="inputZip"
                           value={zip}
-                          onChange={e => setZip(e.target.value)}
+                          onChange={(e) => setZip(e.target.value)}
                         />
                       </div>
                       <div className="form-group">
@@ -303,14 +288,18 @@ function Projects() {
                           id="inputToDo"
                           placeholder="List of To-Do's"
                           value={todo}
-                          onChange={e => setToDo(e.target.value)}
+                          onChange={(e) => setToDo(e.target.value)}
                         />
                       </div>
                     </div>
                   </form>
                 </div>
                 <div className="modal-footer">
-                  <button type="submit" id="toDoBtn" className="btn btn-success">
+                  <button
+                    type="submit"
+                    id="toDoBtn"
+                    className="btn btn-success"
+                  >
                     Add To Do
                   </button>
                   <button
@@ -318,7 +307,6 @@ function Projects() {
                     id="finishBtn"
                     className="btn btn-primary"
                     data-dismiss="modal"
-                    onclick={handleProjectCard}
                   >
                     Finish
                   </button>
@@ -334,11 +322,7 @@ function Projects() {
           <div className="card mb-3">
             <div className="row no-gutters">
               <div className="col-md-4">
-                <img
-                  src="sample-house.jpg"
-                  className="card-img"
-                  alt="..."
-                />
+                <img src="sample-house.jpg" className="card-img" alt="..." />
               </div>
               <div className="col-md-8">
                 <div className="card-body">
@@ -391,12 +375,8 @@ function Projects() {
                       <div className="dropdown-item text-success">
                         Completed
                       </div>
-                      <div className="dropdown-item text-info">
-                        In Progress
-                      </div>
-                      <div className="dropdown-item text-warning">
-                        Bidded
-                      </div>
+                      <div className="dropdown-item text-info">In Progress</div>
+                      <div className="dropdown-item text-warning">Bidded</div>
                     </div>
                   </div>
                 </div>
@@ -407,11 +387,7 @@ function Projects() {
           <div className="card mb-3">
             <div className="row no-gutters">
               <div className="col-md-4">
-                <img
-                  src="sample-house.jpg"
-                  className="card-img"
-                  alt="..."
-                />
+                <img src="sample-house.jpg" className="card-img" alt="..." />
               </div>
               <div className="col-md-8">
                 <div className="card-body">
@@ -464,12 +440,8 @@ function Projects() {
                       <div className="dropdown-item text-success">
                         Completed
                       </div>
-                      <div className="dropdown-item text-info">
-                        In Progress
-                      </div>
-                      <div className="dropdown-item text-warning">
-                        Bidded
-                      </div>
+                      <div className="dropdown-item text-info">In Progress</div>
+                      <div className="dropdown-item text-warning">Bidded</div>
                     </div>
                   </div>
                 </div>
@@ -480,11 +452,7 @@ function Projects() {
           <div className="card mb-3">
             <div className="row no-gutters">
               <div className="col-md-4">
-                <img
-                  src="sample-house.jpg"
-                  className="card-img"
-                  alt="..."
-                />
+                <img src="sample-house.jpg" className="card-img" alt="..." />
               </div>
               <div className="col-md-8">
                 <div className="card-body">
@@ -537,12 +505,8 @@ function Projects() {
                       <div className="dropdown-item text-success">
                         Completed
                       </div>
-                      <div className="dropdown-item text-info">
-                        In Progress
-                      </div>
-                      <div className="dropdown-item text-warning">
-                        Bidded
-                      </div>
+                      <div className="dropdown-item text-info">In Progress</div>
+                      <div className="dropdown-item text-warning">Bidded</div>
                     </div>
                   </div>
                 </div>
