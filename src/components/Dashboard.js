7bd4e249d891cc2404455356158c85f736b96125
BryceStockman <65779581+BryceStockman@users.noreply.github.com>
2022-03-29 19:50:32 -0600

    commitheader and nav

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,37 +1,45 @@
-import React, { useState } from 'react'
-import { Card, Button, Alert } from 'react-bootstrap'
-import { Link, useNavigate } from 'react-router-dom'
-import { useAuth } from '../contexts/AuthContext'
+import React, { useState } from 'react';
+import { Card, Button, Alert } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+import Header from '../components/Header';
 
 export default function Dashboard() {
-  const [error, setError] = useState("")
-  const { currentUser, logout } = useAuth()
-  const navigate  = useNavigate()
+  const [error, setError] = useState('');
+  const { currentUser, logout } = useAuth();
+  const navigate = useNavigate();
 
   async function handleLogout() {
-    setError('')
+    setError('');
 
     try {
-      await logout()
-      navigate('/login')
+      await logout();
+      navigate('/login');
     } catch {
-      setError('Failed to log out')
+      setError('Failed to log out');
     }
   }
   return (
     <>
+      <Header></Header>
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Profile</h2>
           {error && <Alert variant="danger">{error}</Alert>}
           <strong>Email: </strong> {currentUser?.email}
-          <Link to="/update-profile" className="btn btn-outline-danger w-100 mt-3">Update Profile</Link>
+          <Link
+            to="/update-profile"
+            className="btn btn-outline-danger w-100 mt-3"
+          >
+            Update Profile
+          </Link>
         </Card.Body>
-      
       </Card>
-      <div className ="w-100 text-center mt-2">
-        <Button type="button" className="btn btn-danger" onClick={handleLogout}>Log Out</Button>  
+      <div className="w-100 text-center mt-2">
+        <Button type="button" className="btn btn-danger" onClick={handleLogout}>
+          Log Out
+        </Button>
       </div>
     </>
-  )
+  );
 }
