f54470ce946fb3bdf31139aa1ad05c3180f1d158
Kayce Heap <kayce.heap@gmail.com>
2022-03-31 15:39:56 -0600

    updated folder structure and file location for react and mongodb

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,29 +1,26 @@
-import React, { Component } from 'react'
-import { Routes, Route, PrivateRoute } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
-import Signup from './Signup'
-import Dashboard from './Dashboard'
-import Login from './Login'
-import { AuthProvider } from "../contexts/AuthContext";
+import React, { Component } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Signup from './Signup';
+import Dashboard from './Dashboard';
+import Login from './Login';
+import PrivateRoute from './PrivateRoute';
+import ForgotPassword from './ForgotPassword';
+import { AuthProvider } from '../contexts/AuthContext';
+
 
 
 function App() {
   return (
     <AuthProvider>
-      <Container className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh "}}>
-        <div className="w-100" style= {{maxWidth: '400px'}}>
-            <AuthProvider>
-              <Routes>
-                <Route exact path="/" element={<Dashboard />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/login" element={<Login />} />
-              </Routes>
-            </AuthProvider>
-        </div>
-      </Container>
+      <Routes>
+        <Route path="/" element={<PrivateRoute component={Dashboard} />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Routes>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
