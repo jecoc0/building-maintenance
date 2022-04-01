bf4eefb3ab298bb0b14207c98745a1ee0684d98e
BryceStockman <65779581+BryceStockman@users.noreply.github.com>
2022-03-31 19:17:48 -0600

    nav links

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,8 +7,9 @@ import Login from './Login';
 import PrivateRoute from './PrivateRoute';
 import ForgotPassword from './ForgotPassword';
 import { AuthProvider } from '../contexts/AuthContext';
-
-
+import Overview from './Overview';
+import Projects from './Projects';
+import Contact from './Contact';
 
 function App() {
   return (
@@ -18,6 +19,15 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route
+          path="/overview"
+          element={<PrivateRoute component={Overview} />}
+        />
+        <Route
+          path="/projects"
+          element={<PrivateRoute component={Projects} />}
+        />
+        <Route path="/contact" element={<PrivateRoute component={Contact} />} />
       </Routes>
     </AuthProvider>
   );
