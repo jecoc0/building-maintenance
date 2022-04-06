a46038a0c4ae00fe7c89156f8a4719d760d91466
Jessica <sevorg.jess@gmail.com>
2022-04-05 22:17:17 -0600

    heroku

--- server/server.js
+++ server/server.js
@@ -27,6 +27,9 @@ const startServer = async () => {
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
